74b61b3161dbe0722f2286a49f0d8709d60ee6e7
elik <elik@example.com>
2021-06-04 15:29:12 +0300

    Load others files

---
+++ App.Lib/Models/BoardModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apps.Lib.Models
+{
+    public class BoardModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле BoardKey не може бути пустим!"), StringLength(255)]
+        public string BoardKey { get; set; }
+        [Required(ErrorMessage = "Поле UrlImage не може бути пустим!"), StringLength(255)]
+        public string UrlImage { get; set; }
+
+
+    }
+}
---
+++ App.Lib/Models/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apps.Lib.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public int BoardId { get; set; }
+        [Required(ErrorMessage = "Поле TaskText не може бути пустим!"), StringLength(255)]
+        public string TaskText { get; set; }
+        [Required(ErrorMessage = "Поле Comment не може бути пустим!"), StringLength(4000)]
+        public string Comment { get; set; }
+    }
+}
